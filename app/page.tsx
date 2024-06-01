e65390861bb79686e2fb005681cff67aa77fbdad
seanyew25 <seanyew25@gmail.com>
2024-06-01 13:28:03 +0800

    Edited the Home function such that it can take in variables from javascript

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { getClient } from "@/_lib/apolloClient";
 import { gql } from "@apollo/client/core";
+import { create } from "domain";
+
+//TODO
+//create routing
+//implement error handling
 
 export default async function Home() {
   const client = getClient();
+  const username = "zhiwei";
 
   const userQuery = gql`
-    query MyQuery {
-      users(where: { email: "zhiwei@email.com" }) {
+    query MyQuery($username: String!) {
+      users(where: { username: $username }) {
         email
         id
         username
@@ -16,16 +22,4 @@ export default async function Home() {
       }
     }
   `;
-
-  // const { data, loading, error } = useQuery({ query: GET_DOGS });
-  const { data } = await client.query({ query: userQuery });
-  console.log(data);
-
-  return (
-    // <ApolloProvider client={createApolloClient()}>
-    <div>
-      <h2>My first Apollo app</h2>
-    </div>
-    // </ApolloProvider>
-  );
 }
