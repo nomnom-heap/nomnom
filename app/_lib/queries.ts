5198e0a89f51bd61b355093c522a50df32b26acb
Darren-0 <yyodarren@yahoo.com.sg>
2024-06-05 20:44:22 +0800

    function to get recipe, still need to include way to retrieve current user

---
+++ app/_lib/queries.ts
@@ -0,0 +1,21 @@
+import { getClient, query } from "@/_lib/apolloClient";
+import { gql } from "@apollo/client/core";
+
+
+  
+const GET_USER_OWNED_RECIPES = gql`
+  query GetUserOwnedRecipes($userId: ID!) {
+    users(where: { id: $userId }) {
+      id
+      username
+      owns {
+        name
+        contents
+        createdAt
+        ingredients
+        thumbnail_url
+        time_taken_mins
+      }
+    }
+  }
+`;
