373e30ead5cb9d8777920af5ed8ace724551e939
seanyew25 <seanyew25@gmail.com>
2024-06-28 14:28:54 +0800

    Moved the Modal code over to the RecipeCard component file

--- app/_components/RecipeCard.tsx
+++ app/_components/RecipeCard.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useDisclosure } from "@nextui-org/react";
 import {
   Card,
   CardHeader,
@@ -17,6 +18,13 @@ import {
   RefetchQueriesFunction,
 } from "@apollo/client";
 import { gql } from "@apollo/client/core";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@nextui-org/modal";
 
 export function RecipeCard({ recipeObj, userId }) {
   // console.log(userId);
@@ -101,54 +109,96 @@ export function RecipeCard({ recipeObj, userId }) {
     setLike(checkUserFav);
   }, []);
 
-  return (
-    <Card className="relative group">
-      <CardHeader className="pb-0 pt-3 px-3 m-2 flex-col items-start">
-        <h4 className="font-bold text-lg">{recipeObj.name}</h4>
-      </CardHeader>
-      <CardBody className="p-3 justify-end position: static object-fit: cover">
-        <a href="#">
-          <Image
-            isZoomed
-            radius="lg"
-            width="100%"
-            alt="Card background"
-            className="object-cover rounded-xl h-[200px] w-full"
-            src={recipeObj.thumbnail_url}
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        </a>
-      </CardBody>
-      <CardFooter className="pt-0 px-3 mb-0 justify-between">
-        <div className="grid-flow-row pb-1 space-y-0.5">
-          <p
-            className="mt-2 text-sm text-gray-500"
-            style={{ alignSelf: "flex-end" }}
-          >
-            🕛 {recipeObj.time_taken_mins} mins
-          </p>
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-          <p
-            className="mt-2 text-sm text-gray-500"
-            style={{ alignSelf: "flex-end" }}
-          >
-            🍽️ {recipeObj.serving} servings
-          </p>
-        </div>
-        <Button
-          isIconOnly
-          color="danger"
-          aria-label="Like"
-          onClick={() => {
-            setLike((value) => !value);
-            like
-              ? handleUnfavouriteRecipe(recipeObj.id)
-              : handleFavouriteRecipe(recipeObj.id);
-          }}
-        >
-          <HeartIcon filled={like} />
-        </Button>
-      </CardFooter>
-    </Card>
+  return (
+    <>
+      <div className="cursor-pointer" onClick={onOpen}>
+        <Card className="relative group">
+          <CardHeader className="pb-0 pt-3 px-3 m-2 flex-col items-start">
+            <h4 className="font-bold text-lg">{recipeObj.name}</h4>
+          </CardHeader>
+          <CardBody className="p-3 justify-end position: static object-fit: cover">
+            <a href="#">
+              <Image
+                isZoomed
+                radius="lg"
+                width="100%"
+                alt="Card background"
+                className="object-cover rounded-xl h-[200px] w-full"
+                src={recipeObj.thumbnail_url}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+            </a>
+          </CardBody>
+          <CardFooter className="pt-0 px-3 mb-0 justify-between">
+            <div className="grid-flow-row pb-1 space-y-0.5">
+              <p
+                className="mt-2 text-sm text-gray-500"
+                style={{ alignSelf: "flex-end" }}
+              >
+                🕛 {recipeObj.time_taken_mins} mins
+              </p>
+
+              <p
+                className="mt-2 text-sm text-gray-500"
+                style={{ alignSelf: "flex-end" }}
+              >
+                🍽️ {recipeObj.serving} servings
+              </p>
+            </div>
+
+            <Button
+              isIconOnly
+              color="danger"
+              aria-label="Like"
+              onClick={() => {
+                setLike((value) => !value);
+                like
+                  ? handleUnfavouriteRecipe(recipeObj.id)
+                  : handleFavouriteRecipe(recipeObj.id);
+              }}
+            >
+              <HeartIcon filled={like} />
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent className="bg-gray-300">
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                <p> </p>
+                <p> </p>
+                <p> </p>
+                <p> </p>
+                <p> </p>
+                <p>
+                  <Image
+                    src={recipeObj.thumbnail_url}
+                    alt={recipeObj.name}
+                    style={{ width: "400px", height: "300px" }}
+                  />
+                </p>
+                {recipeObj.name}
+              </ModalHeader>
+              <ModalBody>
+                <p>Preparation Time:🕛 {recipeObj.time_taken_mins} mins</p>
+                <ul>
+                  <p>Ingredients:</p>
+                  {recipeObj.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                  ))}
+                </ul>
+                <p>Steps:</p>
+                <p>{recipeObj.contents}</p>
+              </ModalBody>
+              <ModalFooter></ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,6 @@ import {
   RefetchQueriesFunction,
 } from "@apollo/client";
 import { gql } from "@apollo/client/core";
-import { Autocomplete, AutocompleteItem } from "@nextui-org/autocomplete";
-import { AutocompleteSection } from "@nextui-org/autocomplete";
 
 // Import local components and utilities
 import RootLayout from "./layout";
@@ -19,14 +17,6 @@ import { getClient, query } from "@/_lib/apolloClient";
 
 // Import hooks from React
 import { useState, useMemo, useEffect } from "react";
-import { useDisclosure } from "@nextui-org/react";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "@nextui-org/modal";
 
 import { fetchAuthSession } from "aws-amplify/auth";
 import { SearchFunction } from "./_components/SearchFunction";
@@ -141,163 +131,5 @@ function SortBar() {
       <Checkbox size="md">Time Taken</Checkbox>
       <Checkbox size="md">Preparation Time</Checkbox>
     </div>
-  );
-}
-
-const GET_INGREDIENTS_QUERY = gql`
-  query MyQuery {
-    ingredients {
-      name
-      value
-    }
-  }
-`;
-
-function IngredientSearchBar() {
-  const { data, loading, error } = useQuery(GET_INGREDIENTS_QUERY);
-  const [ingredient, setIngredient] = useState([]);
-  const handleSelectionChange = (selectedItem) => {
-    if (!ingredient.includes(selectedItem) && selectedItem != null) {
-      setIngredient([...ingredient, selectedItem]);
-      console.log(ingredient);
-    } else {
-      console.log("duplicate");
-    }
-  };
-
-  const [selectedKeys, setSelectedKeys] = useState(new Set(["text"]));
-  const selectedValue = useMemo(
-    () => Array.from(selectedKeys).join(", "),
-    [selectedKeys]
-  );
-
-  if (loading) {
-    return <p>Loading ingredients...</p>;
-  }
-  const ingredients = data.ingredients;
-  return (
-    <>
-      <Autocomplete
-        label="Ingredients"
-        placeholder="Search an ingredient"
-        className="max-w-screen"
-        disableSelectorIconRotation
-        selectorIcon={<SearchIcon />}
-        onSelectionChange={handleSelectionChange}
-      >
-        {ingredients.map((item) => (
-          <AutocompleteItem key={item.value} value={item.value}>
-            {item.name}
-          </AutocompleteItem>
-        ))}
-      </Autocomplete>
-
-      {ingredient.length > 0 && (
-        <div className="flex flex-col gap-2">
-          <h4>Ingredients so far</h4>
-          <ListboxWrapper>
-            <Listbox
-              aria-label="Single selection example"
-              variant="flat"
-              disallowEmptySelection
-              selectionMode="single"
-              selectedKeys={selectedKeys}
-              onSelectionChange={setSelectedKeys}
-            >
-              {ingredient.map((x) => {
-                let ingredientObj = ingredients.find((el) => el.value === x);
-                return <ListboxItem key={x}>{ingredientObj.name}</ListboxItem>;
-              })}
-            </Listbox>
-          </ListboxWrapper>
-          <span>
-            <Button
-              isIconOnly
-              color="danger"
-              aria-label="Like"
-              onClick={() => {
-                setIngredient(ingredient.filter((i) => i !== selectedValue));
-              }}
-            >
-              <DeleteIcon />
-            </Button>
-          </span>
-        </div>
-      )}
-    </>
-  );
-}
-
-function RecipeCard({ recipeObj }) {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-  return (
-    <>
-      <div className="cursor-pointer" onClick={onOpen}>
-        <Card className="p-4 w-80 h-26">
-          <CardHeader className="pb-0 pt-2 px-4 m-2 flex-col items-start">
-            <h4 className="font-bold text-large">{recipeObj.name}</h4>
-          </CardHeader>
-          <CardBody className="p-2 justify-end position: static object-fit: cover">
-            <Image
-              isZoomed
-              radius="lg"
-              width="100%"
-              alt="Card background"
-              className="object-cover rounded-xl h-[200px] w-full"
-              src={recipeObj.thumbnail_url}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
-          </CardBody>
-          <CardFooter className="justify-between">
-            <p
-              className="mt-2 self-end text-md text-gray-500"
-              style={{ alignSelf: "flex-end" }}
-            >
-              🕛 {recipeObj.time_taken_mins} mins
-            </p>
-            <Button isIconOnly color="danger" aria-label="Like">
-              <HeartIcon />
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
-
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent className="bg-gray-300">
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                <p> </p>
-                <p> </p>
-                <p> </p>
-                <p> </p>
-                <p> </p>
-                <p>
-                  <Image
-                    src={recipeObj.thumbnail_url}
-                    alt={recipeObj.name}
-                    style={{ width: "400px", height: "300px" }}
-                  />
-                </p>
-                {recipeObj.name}
-              </ModalHeader>
-              <ModalBody>
-                <p>Preparation Time:🕛 {recipeObj.time_taken_mins} mins</p>
-                <ul>
-                  <p>Ingredients:</p>
-                  {recipeObj.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                  ))}
-                </ul>
-                <p>Steps:</p>
-                <p>{recipeObj.contents}</p>
-              </ModalBody>
-              <ModalFooter></ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
   );
 }
