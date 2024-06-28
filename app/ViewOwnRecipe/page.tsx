1f1c6ffef2901c6f0460caf26dbacf4a1c570708
seanyew25 <seanyew25@gmail.com>
2024-06-28 13:41:15 +0800

    Added reusuable components: RecipeContainer, RecipeCards to the viewOwnRecipe page. Modified the userId fetching to be dynamic. Changed loading and error handling for the graphql requests.

--- app/ViewOwnRecipe/page.tsx
+++ app/ViewOwnRecipe/page.tsx
@@ -2,9 +2,9 @@
 // import { getClient } from "@/_lib/apolloClient";
 import { useEffect, useState } from "react";
 import { Button, Image } from "@nextui-org/react";
-import { HeartIcon } from "./HeartIcon";
+import { HeartIcon } from "../_components/HeartIcon";
 import { PiPencilLine } from "react-icons/pi";
-import { gql } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 
 import { RecipeContainer } from "../_components/RecipeContainer";
 import { fetchAuthSession } from "aws-amplify/auth";
@@ -15,12 +15,17 @@ const GET_USER_OWNED_RECIPES = gql`
       id
       display_name
       recipes {
+        id
         name
         contents
         createdAt
         ingredients
         thumbnail_url
         time_taken_mins
+        serving
+        favouritedByUsers {
+          id
+        }
       }
     }
   }
@@ -28,37 +33,49 @@ const GET_USER_OWNED_RECIPES = gql`
 
 function UserOwnedRecipes({ userId }) {
   // const userId = "89fa054c-2071-7009-5a38-05265e229ccd";
-  const { data, error } = await client.query({
-    query: GET_USER_OWNED_RECIPES,
+  const [userRecipes, setUserRecipes] = useState("");
+
+  const { data, loading, error } = useQuery(GET_USER_OWNED_RECIPES, {
     variables: { userId },
   });
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-  if (!data) {
+  // if (error) {
+  //   return <p>Error: {error.message}</p>;
+  // }
+  // if (!data) {
+  //   return <p>Loading...</p>;
+  // }
+
+  useEffect(() => {
+    if (loading) setUserRecipes("loading recipes");
+
+    if (data) setUserRecipes(data.users[0]);
+
+    if (error) console.error(error);
+  }, [loading, data, error]);
+
+  // const recipeCount = userRecipes.length;
+
+  if (userRecipes === "loading recipes" || userRecipes === "")
     return <p>Loading...</p>;
+  else {
+    return (
+      <div>
+        <h1>{data.users[0].display_name}'s Recipes</h1>
+        <p>Number of recipes: {userRecipes.length}</p>
+        <div className="border-2 border-black rounded-full flex items-end mb-4">
+          <p className="ml-auto mr-2">My Favourites😊</p>
+        </div>
+        <div className="flex justify-end">
+          <Button color="" className="mr-2">
+            <span className="relative mr-2">Create Recipe</span>
+            <PiPencilLine className="text-gray-400" />
+          </Button>
+        </div>
+        <RecipeContainer recipeResult={userRecipes} userId={userId} />
+      </div>
+    );
   }
-
-  const recipes = data.users[0].recipes;
-  const recipeCount = recipes.length;
-
-  return (
-    <div>
-      <h1>{data.users[0].display_name}'s Recipes</h1>
-      <p>Number of recipes: {recipeCount}</p>
-      <div className="border-2 border-black rounded-full flex items-end mb-4">
-        <p className="ml-auto mr-2">My Favourites😊</p>
-      </div>
-      <div className="flex justify-end">
-        <Button color="" className="mr-2">
-          <span className="relative mr-2">Create Recipe</span>
-          <PiPencilLine className="text-gray-400" />
-        </Button>
-      </div>
-      <RecipeContainer recipeResult={data} userId={userId} />
-    </div>
-  );
 }
 
 const App = () => {
