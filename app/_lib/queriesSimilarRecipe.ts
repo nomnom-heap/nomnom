9829b94899cc20e51d868c4ba982a41fa12698b9
Darren-0 <yyodarren@yahoo.com.sg>
2024-07-01 16:56:15 +0800

    updated the queries to get similar recipes

--- app/_lib/queriesSimilarRecipe.ts
+++ app/_lib/queriesSimilarRecipe.ts
@@ -1,46 +1,46 @@
 //setup driver
 import neo4j from "neo4j-driver";
-import { Neo4jGraphQL } from "@neo4j/graphql";
-//import { gql, useQuery } from "@apollo/client";
+const driver = neo4j.driver(
+  process.env.NEO4J_URI!,
+  neo4j.auth.basic(process.env.NEO4J_USER!, process.env.NEO4J_PASSWORD!)
+);
 
-const createFullTextIndex = /* GraphQL */`
-  CREATE FULLTEXT INDEX $name FOR (n:Recipe) ON EACH [n.id, n.ingredients]
-  OPTIONS {
-    indexConfig: {
-      \`fulltext.analyzer\`: 'english',
-      \`fulltext.eventually_consistent\`: true
-    }
-  }
-`
-async function getSimilarRecipes() {
+export async function createFullTextIndex() {
   const session = driver.session();
-
-  const query = `
-    MATCH (n:Recipe2 {name : $name}) 
-    CALL db.index.vector.queryNodes("recipe2Ingredients", 2, n.embedding)
-    YIELD node AS recipe
-    WHERE recipe <> n
-    RETURN recipe.name AS name, recipe.ingredients as ingredients
-  `;
-
-  const params = { name: 'omelette' };
-
+  const indexName = "fullStackIngredients";
+  
   try {
-    const result = await session.run(query, params);
-
-    // Process the results
-    const recipes = result.records.map(record => ({
-      name: record.get('name'),
-      ingredients: record.get('ingredients')
-    }));
-
-    return recipes;
-  } catch (error) {
-    console.error('Error executing query:', error);
-    throw error;
+    await session.run(/* GraphQL */`
+      CREATE FULLTEXT INDEX ${indexName} FOR (n:Recipe) ON EACH [n.id, n.ingredients] 
+      IF NOT EXISTS
+      OPTIONS {
+        indexConfig: {
+          'fulltext.analyzer': 'english',
+          'fulltext.eventually_consistent': true
+        }
+      }
+    `);
   } finally {
     await session.close();
   }
 }
 
-export default getSimilarRecipes;
+export async function findRecipesByIngredients(ingredientString: string) {
+  const session = driver.session();
+  
+  try {
+    const result = await session.run(/* GraphQL */`
+      CALL db.index.fulltext.queryNodes("fullStackIngredients", $ingredientString) 
+      YIELD node, score
+      WHERE node <> $ingredientString
+      RETURN node.name AS name, node.ingredients AS ingredients
+    `, { ingredientString });
+
+    return result.records.map(record => ({
+      name: record.get('name'),
+      ingredients: record.get('ingredients')
+    }));
+  } finally {
+    await session.close();
+  }
+}
