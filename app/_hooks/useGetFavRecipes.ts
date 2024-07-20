ba4ef099ab795dceef64ed9929973a969a99bd70
mightymagnus <theanzhiwei@gmail.com>
2024-07-21 05:13:38 +0800

    feat: favourites page

--- app/_hooks/useGetFavRecipes.ts
+++ app/_hooks/useGetFavRecipes.ts
@@ -1,40 +1,12 @@
 import { gql, useLazyQuery } from "@apollo/client";
-import { useState, useEffect } from "react";
-// import { useAuth } from "../AuthProvider";
-import { fetchAuthSession } from "aws-amplify/auth";
-
-// interface Recipe {
-//   id: string;
-//   name: string;
-//   ingredients: string[];
-//   ingredients_qty: string[];
-//   serving: number;
-//   time_taken_mins: number;
-//   thumbnail_url: string;
-//   contents: string;
-//   owner: {
-//     id: string;
-//     display_name: string;
-//   };
-//   favouritedByUsers: { id: string }[];
-// }
-
-// interface User {
-//   id: string;
-//   display_name: string;
-//   favourite_recipes: Recipe[];
-// }
 
 interface GetFavRecipesData {
   users: User[];
 }
 
-// const { userId } = useAuth();
-// console.log(userId);
-
 const GET_FAV_RECIPES_QUERY = gql`
   query getFavRecipes($userId: ID!) {
-    users(where: { id_CONTAINS: $userId }) {
+    users(where: { id: $userId }) {
       id
       favourite_recipes {
         id
@@ -48,6 +20,9 @@ const GET_FAV_RECIPES_QUERY = gql`
         owner {
           id
           display_name
+          followers {
+            id
+          }
         }
         favouritedByUsers {
           id
@@ -57,52 +32,14 @@ const GET_FAV_RECIPES_QUERY = gql`
   }
 `;
 
-export default function useGetFavRecipes(limit: number = 9) {
-  // const { userId } = useAuth();
-  // console.log(userId);
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [totalPages, setTotalPages] = useState<number>(0);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
-  const [
-    getFavRecipes,
-    {
-      data: getRecipesData,
-      loading: getRecipesLoading,
-      error: getRecipesError,
-    },
-  ] = useLazyQuery<GetFavRecipesData>(GET_FAV_RECIPES_QUERY);
-
-  useEffect(() => {
-    if (getRecipesData) {
-      console.log(getRecipesData);
-      const allFavRecipes = getRecipesData.users.flatMap(
-        (user) => user.favourite_recipes
-      );
-      const totalPages = Math.ceil(allFavRecipes.length / limit);
-      setTotalPages(totalPages);
-      setRecipes((prevRecipes) => [
-        ...prevRecipes,
-        ...allFavRecipes.slice((currentPage - 1) * limit, currentPage * limit),
-      ]);
-    }
-  }, [getRecipesData, currentPage, limit]);
-
-  useEffect(() => {
-    async function getUserId() {
-      const session = await fetchAuthSession();
-      const userId = session?.tokens?.accessToken.payload.sub;
-      getFavRecipes({ variables: { userId: userId } });
-    }
-    getUserId();
-  }, []);
+export default function useGetFavRecipes() {
+  const [getFavRecipes, { data, loading, error }] =
+    useLazyQuery<GetFavRecipesData>(GET_FAV_RECIPES_QUERY);
 
   return {
-    recipes,
-    totalPages,
-    currentPage,
-    setCurrentPage,
-    loading: getRecipesLoading,
-    error: getRecipesError,
+    getFavRecipes,
+    data,
+    loading,
+    error,
   };
 }
